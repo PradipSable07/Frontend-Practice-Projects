5e431fa3d9eff901d029d898beea407ca67de1a2
Pradip Sable <96158817+PradipSable07@users.noreply.github.com>
2024-10-18 14:28:04 +0530

    modify [data.js]: added new links for code

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ export const projectData = [
     description: "This project is a preview of a blog card component. It can be used on a blog page to display blog posts.",
     image: "./assets/Blog-Preview-page.avif",
     liveLink: "./blog-preview-card-main/index.html",
-    codeLink: ""
+    codeLink: "https://github.com/PradipSable07/Frontend-Practice-Projects/tree/main/blog-preview-card-main"
   },
   {
     title: "QR Code Component",
     description: "A QR code component preview for different use cases. It can be used for landing pages and information sharing.",
     image: "./assets/QR-main.avif",
     liveLink: "./qr-code-component-main/index.html",
-    codeLink: "."
+    codeLink: "https://github.com/PradipSable07/Frontend-Practice-Projects/tree/main/qr-code-component-main"
   },
   {
     title: "Profile Card Component",
     description: "This project is a preview of a profile card component. It can be used on a profile page to display profile information.",
     image: "./assets/Screenshot-2024-10-18-at-09.58.22.avif",
     liveLink: "./profile-card-component-main/index.html",
-    codeLink: ""
+    codeLink: "https://github.com/PradipSable07/Frontend-Practice-Projects/tree/main/profile-card-component-main"
   }
 ];
