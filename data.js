20de7c3dfd247fd0ac1824280be97636dd465533
pradipNCS <pradip.nucleosys@gmail.com>
2024-10-18 12:41:59 +0530

    feat: created new Project with the some mini project for practice here i have created dynamic card functionality with json data

---
+++ data.js
@@ -0,0 +1,25 @@
+// JSON Data for projects
+export const projectData = [
+    {
+      title: "Blog Preview Card",
+      description: "This project is a preview of a blog card component. It can be used on a blog page to display blog posts.",
+      image: "./assets/Blog-Preview-page.avif",
+      liveLink: "./blog-preview-card-main/index.html",
+      codeLink: ""
+    },
+    {
+      title: "QR Code Component",
+      description: "A QR code component preview for different use cases. It can be used for landing pages and information sharing.",
+      image: "./assets/QR-main.avif",
+      liveLink: "./qr-code-component-main/index.html",
+      codeLink: "."
+    },
+    {
+        title: "Profile Card Component",
+        description: "This project is a preview of a profile card component. It can be used on a profile page to display profile information.",
+        image:"./assets/Screenshot-2024-10-18-at-09.58.22.avif",
+        liveLink:"./profile-card-component-main/index.html",
+        codeLink:""
+    }
+  ];
+  
