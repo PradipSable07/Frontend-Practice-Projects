9e7c94ef64cde87cb0ed87ca3c57a614a4f147cf
PradipSable07 <sablepradipsubhash@gmail.com>
2024-10-18 14:13:09 +0530

    modify [index.html, index.js, style.css, data.js]: modify the card added functionality

--- index.js
+++ index.js
@@ -1,83 +1,119 @@
-import { projectData } from "./data";
-  // Select the projects section
-  const projectSection = document.querySelector('.projects');
-  
-  // Function to create and append cards
-  const createCard = (project) => {
-    const cardHTML = 	`<div
-    class="card">
-    
-    <img
-        class="card-img"
-        src=${project.image}
-        alt=${project.title} />
+import { projectData } from "./data.js"; // Import data
 
-    <h2 class="card-heading">${project.title}</h2>
-    <p class="card-description">
-        ${project.description}
-    </p>
-    <div class="card-btn-container">
-        
-        <a href=${project.liveLink} class="button button-live" target="_blank">
-            <span class="button__icon-wrapper">
-                <svg
-                    viewBox="0 0 14 15"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="button__icon-svg"
-                    width="10">
-                    <path
-                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                        fill="currentColor"></path>
-                </svg>
+// Select the projects section
+const projectSection = document.querySelector(".projects");
 
-                <svg
-                    viewBox="0 0 14 15"
-                    fill="none"
-                    width="10"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="button__icon-svg button__icon-svg--copy">
-                    <path
-                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                        fill="currentColor"></path>
-                </svg>
-            </span>
-            See Live 
-        </a>
-        <a href=${project.codeLink} class="button" target="_blank" >
-            <span class="button__icon-wrapper">
-                <svg
-                    viewBox="0 0 14 15"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="button__icon-svg"
-                    width="10">
-                    <path
-                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                        fill="currentColor"></path>
-                </svg>
+// Function to create and append cards
+const createCard = (project) => {
+    // Create card elements
+    const card = document.createElement("div");
+    card.className = "card";
 
-                <svg
-                    viewBox="0 0 14 15"
-                    fill="none"
-                    width="10"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="button__icon-svg button__icon-svg--copy">
-                    <path
-                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-                        fill="currentColor"></path>
-                </svg>
-            </span>
-            Code
-        </a>
+    const img = document.createElement("img");
+    img.className = "card-img";
+    img.src = project.image;
+    img.alt = project.title;
 
-        
-    </div>
-</div>`
-  
-    // Append card to project section
-    projectSection.innerHTML+=(cardHTML);
-  };
-  
-  // Loop through project data and create cards
-  projectData.forEach(createCard);
+    const heading = document.createElement("h2");
+    heading.className = "card-heading";
+    heading.textContent = project.title;
+
+    const description = document.createElement("p");
+    description.className = "card-description";
+    description.textContent = project.description;
+
+    const btnContainer = document.createElement("div");
+    btnContainer.className = "card-btn-container";
+
+    // Create Live Link button
+    const liveLinkBtn = document.createElement("a");
+    liveLinkBtn.href = project.liveLink;
+    liveLinkBtn.className = "button button-live";
+    liveLinkBtn.target = "_blank";
+
+    const liveLinkSVG = `
+        <span class="button__icon-wrapper">
+    <svg
+      viewBox="0 0 14 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="button__icon-svg"
+      width="10"
+    >
+      <path
+        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+        fill="currentColor"
+      ></path>
+    </svg>
+
+    <svg
+      viewBox="0 0 14 15"
+      fill="none"
+      width="10"
+      xmlns="http://www.w3.org/2000/svg"
+      class="button__icon-svg button__icon-svg--copy"
+    >
+      <path
+        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  </span>
+        See Live`;
+
+    liveLinkBtn.innerHTML = liveLinkSVG;
+
+    // Create Code button
+    const codeBtn = document.createElement("a");
+    codeBtn.href = project.codeLink;
+    codeBtn.className = "button";
+    codeBtn.target = "_blank";
+
+    const codeSVG = `
+        <span class="button__icon-wrapper">
+    <svg
+      viewBox="0 0 14 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="button__icon-svg"
+      width="10"
+    >
+      <path
+        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+        fill="currentColor"
+      ></path>
+    </svg>
+
+    <svg
+      viewBox="0 0 14 15"
+      fill="none"
+      width="10"
+      xmlns="http://www.w3.org/2000/svg"
+      class="button__icon-svg button__icon-svg--copy"
+    >
+      <path
+        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  </span>
+        Code`;
+
+    codeBtn.innerHTML = codeSVG;
+
+    // Append buttons to the button container
+    btnContainer.appendChild(liveLinkBtn);
+    btnContainer.appendChild(codeBtn);
+
+    // Append all elements to the card
+    card.appendChild(img);
+    card.appendChild(heading);
+    card.appendChild(description);
+    card.appendChild(btnContainer);
+
+    // Append card to the project section
+    projectSection.appendChild(card);
+};
+
+// Loop through project data and create cards
+projectData.forEach(createCard);
